f08ca248960bc566f31890413211ef01b66838ba
Lucas Ricardo <lucas.silva@pumpkintech.com.br>
2023-05-28 12:18:31 -0300

    Project structure + local database with docker

---
+++ src/main/java/com/lucasricardo1/CrudExemplo/CrudExemploApplication.java
@@ -0,0 +1,13 @@
+package com.lucasricardo1.CrudExemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudExemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasricardo1/CrudExemplo/CrudExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasricardo1.CrudExemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
