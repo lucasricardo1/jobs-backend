ed5b53c3c7fd26ecbc06fb59f56d4deb2ea8d4cb
Lucas Ricardo <lucas.silva@pumpkintech.com.br>
2023-05-28 15:39:14 -0300

    Repositories creation

--- src/main/java/com/lucasricardo1/CrudExemplo/repositories/ContatoRepository.java
+++ src/main/java/com/lucasricardo1/CrudExemplo/repositories/ContatoRepository.java
@@ -1,2 +1,7 @@
-package com.lucasricardo1.CrudExemplo.repositories;public interface ContatoRepository {
+package com.lucasricardo1.CrudExemplo.repositories;
+
+import com.lucasricardo1.CrudExemplo.entities.ContatoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContatoRepository extends JpaRepository<ContatoEntity, Long> {
 }
--- src/main/java/com/lucasricardo1/CrudExemplo/repositories/ProfissionalRepository.java
+++ src/main/java/com/lucasricardo1/CrudExemplo/repositories/ProfissionalRepository.java
@@ -1,2 +1,7 @@
-package com.lucasricardo1.CrudExemplo.repositories;public interface ProfissionalRepository {
+package com.lucasricardo1.CrudExemplo.repositories;
+
+import com.lucasricardo1.CrudExemplo.entities.ProfissionalEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfissionalRepository extends JpaRepository<ProfissionalEntity, Long> {
 }
