75c2b092fbd0758b61cc2bf273c881c1f50ca9d9
Lucas Ricardo <lucas.silva@pumpkintech.com.br>
2023-05-29 19:27:38 -0300

    Controllers creation

--- src/main/java/com/lucasricardo1/CrudExemplo/controllers/ContatoController.java
+++ src/main/java/com/lucasricardo1/CrudExemplo/controllers/ContatoController.java
@@ -1,2 +1,49 @@
-package com.lucasricardo1.CrudExemplo.controllers;public class ContatoController {
+package com.lucasricardo1.CrudExemplo.controllers;
+
+import com.lucasricardo1.CrudExemplo.dtos.ContatoDTO;
+import com.lucasricardo1.CrudExemplo.mappers.ContatoMapper;
+import com.lucasricardo1.CrudExemplo.services.ContatoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContatoController {
+
+    @Autowired
+    private ContatoService contatoService;
+
+    @Autowired
+    private ContatoMapper contatoMapper;
+
+
+    @GetMapping
+    public ResponseEntity<List<ContatoDTO>> getContatos(@RequestParam(required = false) String q) {
+        return ResponseEntity.ok(ContatoMapper.INSTANCE.toDtos(contatoService.buscarContatos(q)));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContatoDTO> getContatoById(@PathVariable Long id) {
+        return ResponseEntity.ok(ContatoMapper.INSTANCE.toDto(contatoService.buscarContatoPorId(    id)));
+    }
+
+    @PostMapping
+    public ResponseEntity<ContatoDTO> createContato(@RequestBody ContatoDTO contatoDTO) {
+        return ResponseEntity.ok(ContatoMapper.INSTANCE.toDto(contatoService.salvarContato(contatoDTO)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ContatoDTO> updateContato(@RequestBody ContatoDTO contatoDTO) {
+
+        return ResponseEntity.ok(ContatoMapper.INSTANCE.toDto(contatoService.atualizarContato(contatoDTO)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteContato(@PathVariable Long id) {
+        contatoService.deletarContato(id);
+        return ResponseEntity.ok("Contato excluído com sucesso");
+    }
 }
--- src/main/java/com/lucasricardo1/CrudExemplo/controllers/ProfissionalController.java
+++ src/main/java/com/lucasricardo1/CrudExemplo/controllers/ProfissionalController.java
@@ -1,2 +1,57 @@
-package com.lucasricardo1.CrudExemplo.controllers;public class ProfissionalController {
+package com.lucasricardo1.CrudExemplo.controllers;
+
+import com.lucasricardo1.CrudExemplo.dtos.ProfissionalDTO;
+import com.lucasricardo1.CrudExemplo.mappers.ContatoMapper;
+import com.lucasricardo1.CrudExemplo.mappers.ProfissionalMapper;
+import com.lucasricardo1.CrudExemplo.services.ProfissionalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/profissionais")
+public class ProfissionalController {
+
+    @Autowired
+    private ProfissionalService profissionalService;
+
+    @Autowired
+    private ProfissionalMapper profissionalMapper;
+
+
+    @GetMapping
+    public ResponseEntity<List<ProfissionalDTO>> getProfissionais(@RequestParam(required = false) String q,
+                                                  @RequestParam(required = false) List<String> fields) {
+        return ResponseEntity.ok(ProfissionalMapper.INSTANCE.toDtos(profissionalService.buscarProfissional(q)));
+
+    }
+
+    @GetMapping("/{id}")
+    public ProfissionalDTO getProfissionalById(@PathVariable Long id) {
+        return ProfissionalMapper.INSTANCE.toDto(profissionalService.buscarProfissionalPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProfissionalDTO> createProfissional(@RequestBody ProfissionalDTO profissionalDTO) {
+
+        return ResponseEntity.ok(
+                ProfissionalMapper.INSTANCE.toDto(profissionalService.salvarProfissional(profissionalDTO)));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProfissionalDTO> updateProfissional(@PathVariable Long id,
+                                                     @RequestBody ProfissionalDTO profissionalDTO) {
+
+        return ResponseEntity.ok(
+                ProfissionalMapper.INSTANCE.toDto(profissionalService.atualizarProfissional(profissionalDTO)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProfissional(@PathVariable Long id) {
+        profissionalService.deletarProfissional(id);
+        return ResponseEntity.ok("Profissional excluído com sucesso");
+    }
 }
+
