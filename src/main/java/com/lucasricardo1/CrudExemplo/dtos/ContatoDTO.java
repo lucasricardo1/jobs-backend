8b5ac31a2c9c5fbbf4331310d197675caab998d2
Lucas Ricardo <lucas.silva@pumpkintech.com.br>
2023-05-29 12:09:24 -0300

    DTOs update

--- src/main/java/com/lucasricardo1/CrudExemplo/dtos/ContatoDTO.java
+++ src/main/java/com/lucasricardo1/CrudExemplo/dtos/ContatoDTO.java
@@ -1,4 +1,16 @@
 package com.lucasricardo1.CrudExemplo.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContatoDTO {
+
+    private Long id;
+    private String nome;
+    private String contato;
+
 }
--- src/main/java/com/lucasricardo1/CrudExemplo/dtos/ProfissionalDTO.java
+++ src/main/java/com/lucasricardo1/CrudExemplo/dtos/ProfissionalDTO.java
@@ -1,4 +1,23 @@
 package com.lucasricardo1.CrudExemplo.dtos;
 
+import com.lucasricardo1.CrudExemplo.enums.Cargo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProfissionalDTO {
+
+    private Long id;
+    private String nome;
+    private Cargo cargo;
+    private LocalDate nascimento;
+    private LocalDate createData;
+    private List<ContatoDTO> contatos;
+
 }
