3114e743c2bf338566b69057cea1187d07ce8b51
Lucas Ricardo <lucas.silva@pumpkintech.com.br>
2023-05-29 19:29:29 -0300

    Adding business rules

--- src/main/java/com/lucasricardo1/CrudExemplo/services/ContatoService.java
+++ src/main/java/com/lucasricardo1/CrudExemplo/services/ContatoService.java
@@ -2,24 +2,45 @@ package com.lucasricardo1.CrudExemplo.services;
 
 import com.lucasricardo1.CrudExemplo.dtos.ContatoDTO;
 import com.lucasricardo1.CrudExemplo.entities.ContatoEntity;
+import com.lucasricardo1.CrudExemplo.entities.ProfissionalEntity;
 import com.lucasricardo1.CrudExemplo.mappers.ContatoMapper;
 import com.lucasricardo1.CrudExemplo.repositories.ContatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ContatoService {
 
     @Autowired
-    ContatoRepository contatoRepository;
+    private  ContatoRepository contatoRepository;
 
-     public ContatoEntity saveContact(ContatoDTO contatoDTO){
+    public List<ContatoEntity> buscarContatos(String q){
+        return contatoRepository.findContatosByParam(q);
+    }
+
+    public ContatoEntity buscarContatoPorId(Long id){
+        Optional<ContatoEntity> profissionalEntity = contatoRepository.findById(id);
+        if(profissionalEntity.isPresent())
+            return profissionalEntity.get();
+        else return null;
+    }
+
+     public ContatoEntity salvarContato(ContatoDTO contatoDTO){
          return contatoRepository.save(ContatoMapper.INSTANCE.toEntity(contatoDTO));
      }
 
-     public List<ContatoEntity> saveContacts(List<ContatoDTO> contatos){
-        return contatoRepository.saveAll(ContatoMapper.INSTANCE.toEntities(contatos));
+    public ContatoEntity atualizarContato(ContatoDTO contatoDTO){
+        ContatoEntity contatoEntity = contatoRepository.findById(contatoDTO.getId()).get();
+        contatoEntity.setNome(contatoDTO.getNome());
+        contatoEntity.setContato(contatoDTO.getContato());
+
+        return contatoRepository.save(contatoEntity);
+    }
+
+    public void deletarContato(Long id){
+         contatoRepository.deleteById(id);
     }
 }
--- src/main/java/com/lucasricardo1/CrudExemplo/services/ProfissionalService.java
+++ src/main/java/com/lucasricardo1/CrudExemplo/services/ProfissionalService.java
@@ -1,7 +1,72 @@
 package com.lucasricardo1.CrudExemplo.services;
 
+import com.lucasricardo1.CrudExemplo.dtos.ContatoDTO;
+import com.lucasricardo1.CrudExemplo.dtos.ProfissionalDTO;
+import com.lucasricardo1.CrudExemplo.entities.ProfissionalEntity;
+import com.lucasricardo1.CrudExemplo.mappers.ProfissionalMapper;
+import com.lucasricardo1.CrudExemplo.repositories.ContatoRepository;
+import com.lucasricardo1.CrudExemplo.repositories.ProfissionalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProfissionalService {
+
+    @Autowired
+    private ProfissionalRepository profissionalRepository;
+
+    @Autowired
+    private  ContatoRepository contatoRepository;
+
+    @Autowired
+    private  ContatoService contatoService;
+
+    public List<ProfissionalEntity> buscarProfissional(String q){
+        return profissionalRepository.findProfissionaisByParam(q);
+    }
+
+    public ProfissionalEntity buscarProfissionalPorId(Long id){
+        Optional<ProfissionalEntity> profissionalEntity = profissionalRepository.findById(id);
+        if(profissionalEntity.isPresent())
+            return profissionalEntity.get();
+        else return null;
+    }
+
+    public ProfissionalEntity salvarProfissional(ProfissionalDTO profissionalDTO){
+        ProfissionalEntity profissionalEntity =
+                profissionalRepository.save(ProfissionalMapper.INSTANCE.toEntity(profissionalDTO));
+
+        Long profissionalId = profissionalEntity.getId();
+
+        if(profissionalDTO.getContatos() != null && profissionalDTO.getContatos().size() > 0) {
+            for (ContatoDTO contatoDTO : profissionalDTO.getContatos()) {
+                contatoDTO.setProfissionalId(profissionalId);
+                contatoService.salvarContato(contatoDTO);
+            }
+        }
+        return profissionalEntity;
+    }
+
+    public ProfissionalEntity atualizarProfissional(ProfissionalDTO profissionalDTO){
+        ProfissionalEntity profissionalEntity = profissionalRepository.findById(profissionalDTO.getId()).get();
+        profissionalEntity.setNome(profissionalDTO.getNome());
+        profissionalEntity.setCargo(profissionalDTO.getCargo());
+        profissionalEntity.setNascimento(profissionalDTO.getNascimento());
+        profissionalEntity.setCreateData(profissionalDTO.getCreateData());
+
+        if(profissionalDTO.getContatos() != null && profissionalDTO.getContatos().size() > 0){
+            for(ContatoDTO contatoDTO : profissionalDTO.getContatos()){
+                contatoService.atualizarContato(contatoDTO);
+            }
+        }
+
+        return profissionalRepository.save(profissionalEntity);
+    }
+
+    public void deletarProfissional(Long id){
+        profissionalRepository.deleteById(id);
+    }
 }
