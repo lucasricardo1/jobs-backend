9be2a363089e5f11f77d3889106eb17e0abbabb9
Lucas Ricardo <lucas.silva@pumpkintech.com.br>
2023-05-29 19:29:05 -0300

    Fix repositories querys

--- src/main/java/com/lucasricardo1/CrudExemplo/repositories/ContatoRepository.java
+++ src/main/java/com/lucasricardo1/CrudExemplo/repositories/ContatoRepository.java
@@ -2,8 +2,19 @@ package com.lucasricardo1.CrudExemplo.repositories;
 
 import com.lucasricardo1.CrudExemplo.entities.ContatoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ContatoRepository extends JpaRepository<ContatoEntity, Long> {
+
+    @Query(value = "SELECT * FROM Contato c WHERE " +
+            ":busca IS NULL OR " +
+            "c.nome LIKE %:busca% OR " +
+            "c.contato LIKE %:busca%", nativeQuery = true)
+    List<ContatoEntity> findContatosByParam(String busca);
+
 }
--- src/main/java/com/lucasricardo1/CrudExemplo/repositories/ProfissionalRepository.java
+++ src/main/java/com/lucasricardo1/CrudExemplo/repositories/ProfissionalRepository.java
@@ -1,9 +1,21 @@
 package com.lucasricardo1.CrudExemplo.repositories;
 
 import com.lucasricardo1.CrudExemplo.entities.ProfissionalEntity;
+import com.lucasricardo1.CrudExemplo.enums.Cargo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProfissionalRepository extends JpaRepository<ProfissionalEntity, Long> {
+
+    @Query(value = "SELECT * FROM Profissional p WHERE " +
+            ":busca IS NULL OR " +
+            "p.nome LIKE %:busca% OR " +
+            "p.cargo LIKE %:busca% ", nativeQuery = true)
+    List<ProfissionalEntity> findProfissionaisByParam(String busca);
+
 }
